1fcf6184556c3918ee08bb22540f6df1c5e44e72
That python  Kid <mohinathsivaneswaran@gmail.com>
2020-10-10 12:22:02 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "test.h"
+int main(void) {
+  
+  ty("hello there 😀\n", 9);
+  ty("this is cool\n", 9);
+  return 0;
+}
---
+++ test.h
@@ -0,0 +1,10 @@
+int text;
+int loop;
+int ty(text, loop) {
+    
+    int i = loop;
+    for (i = loop; i > 0;) {
+      printf("%s", text);
+      i -= 1;
+    }
+}
